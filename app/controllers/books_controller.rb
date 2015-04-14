63798ff210445ddba23758891f5f4801753f170e
Jason Williams <jasonmw_nf@hotmail.com>
2015-04-14 14:51:06 -0230

    added views for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,30 @@
 class BooksController < ApplicationController
-  
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+
+    if book.save
+      flash[:success] = "Book has been created."
+      redirect_to book_path(book)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  private
+
+    def book_params
+      params.require(:book).permit(:name, :author, :description, :availability)
+    end
 end
