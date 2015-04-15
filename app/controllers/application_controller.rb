39fa17670e8cf90f4e47fc59fd416c0f85dd0ab3
Jason Williams <jasonmw_nf@hotmail.com>
2015-04-15 13:41:17 -0230

    added flash errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_path, danger: "You are not authorized to do that."
+    redirect_to root_path, notice: "You are not authorized to do that."
   end
 end
